3ba1bdaea1b2533a16ef2c587849acfec575030e
Savenok Marta <magnolia.sav@gmail.com>
2023-08-17 22:52:04 +0300

    add

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,8 +1,30 @@
 const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeBurger = document.querySelector('.mobile-head svg');
 
-function activateBurger() {
+function activateBurgerMenu() {
     burger.addEventListener('click' , ()=> {
-        burger.classList.toggle('active');
+        burger.classList.add('active');
+        mobileMenu.classList.add('active');
+        setTimeout(burger.classList.remove('active') , 1000);
     })
 }
-activateBurger();
+function closeBurgerMenu() {
+    closeBurger.addEventListener('click' , ()=> {
+        mobileMenu.classList.remove('active');
+    })
+}
+activateBurgerMenu();
+closeBurgerMenu() ;
+
+const mobileDropdownLinks = document.querySelectorAll('.mobile-dropdown-item');
+
+function activateMobileDropdown() {
+    mobileDropdownLinks.forEach(el => {
+        el.addEventListener('click' , (e)=> {
+            el.classList.toggle('mob-dropdown-open');
+            el.nextElementSibling.classList.toggle('open-mob-submenu');
+        })
+    })
+}
+activateMobileDropdown();
