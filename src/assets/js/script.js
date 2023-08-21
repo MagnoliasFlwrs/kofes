fe8af0f12982417af3be85a43d33553ce11a2981
Savianok Marta <magnolia.sav@gmail.com>
2023-08-21 17:16:42 +0300

    end

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -53,6 +53,21 @@ const swiper = new Swiper('.staff-swiper', {
           spaceBetween: 20
         }
       }
+});
+const teamSwiper = new Swiper('.team-swiper', {
+    slidesPerView: 1.3,
+    spaceBetween: 16,
+    freeMode: true,
+    breakpoints: {
+        768: {
+          slidesPerView: 2.2,
+          spaceBetween: 20
+        }
+    },
+    navigation: {
+        nextEl: '.team-button-next',
+        prevEl: '.team-button-prev',
+      },
    });
 
 
@@ -75,3 +90,105 @@ function boxHandler(e) {
     currentContent.style.maxHeight = 0;
   }
 }
+
+// form mask
+function addMaskFromInputCallBackForm(path, maskValue) {
+  function setCursorPosition(pos, elem) {
+      elem.focus();
+      if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+      else if (elem.createTextRange) {
+          let range = elem.createTextRange();
+          range.collapse(true);
+          range.moveEnd("character", pos);
+          range.moveStart("character", pos);
+          range.select();
+      }
+  }
+
+  function mask(event) {
+      let matrix = maskValue,
+          i = 0,
+          def = matrix.replace(/\D/g, ""),
+          val = this.value.replace(/\D/g, "");
+
+      if (def.length >= val.length) val = def;
+
+      this.value = matrix.replace(/./g, function (a) {
+          return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
+      });
+
+      if (event.type === "blur") {
+          if (this.value.length === 2) this.value = "";
+      } else setCursorPosition(this.value.length, this);
+  }
+
+  let input = document.querySelector(path);
+
+  if (input) {
+
+      input.addEventListener("input", mask, false);
+
+      input.addEventListener("focus", mask, false);
+
+      input.addEventListener("blur", mask, false);
+  }
+
+
+
+}
+console.log(window.location.host)
+
+addMaskFromInputCallBackForm('.input-wrap input[type="tel"]', '+7(___) ___-__-__')
+
+
+
+// modals
+
+const toApplicationBtn = document.querySelectorAll('.to-application');
+const toOrderBtn = document.querySelectorAll('.to-order');
+const applicationModal = document.querySelector('.modal-application');
+const orderModal = document.querySelector('.modal-order');
+const okMessage = document.querySelector('.send-ok-content');
+const overlay =  document.querySelector('.overlay');
+const closeBtns = document.querySelectorAll('.close-inner')
+
+function showModals() {
+    toApplicationBtn.forEach(el => {
+        el.addEventListener('click' , () => {
+            applicationModal.classList.add('open-modal');
+            overlay.classList.add('open');
+        })
+    })
+    toOrderBtn.forEach(el => {
+        el.addEventListener('click' , () => {
+            orderModal.classList.add('open-modal');
+            overlay.classList.add('open');
+        })
+    })
+}
+function closeModals() {
+    closeBtns.forEach(el => {
+        el.addEventListener('click' , (e)=> {
+            if(e.target.closest('.modal-application')) {
+                e.target.closest('.modal-application').classList.remove('open-modal');
+                overlay.classList.remove('open')
+            }
+            if(e.target.closest('.modal-order')) {
+                e.target.closest('.modal-order').classList.remove('open-modal');
+                overlay.classList.remove('open')
+            }
+        })
+    })
+    overlay.addEventListener('click' , ()=> {
+        if(applicationModal) {
+            applicationModal.classList.remove('open-modal');
+            overlay.classList.remove('open')
+        }
+        if(orderModal) {
+            orderModal.classList.remove('open-modal');
+            overlay.classList.remove('open')
+        }
+    })
+}
+showModals();
+closeModals();
