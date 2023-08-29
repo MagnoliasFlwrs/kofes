eade80ebe10e9813010bca87128425587894b6fd
Savianok Marta <magnolia.sav@gmail.com>
2023-08-29 09:41:50 +0300

    fix mobile menu

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -375,5 +375,10 @@ animateBlocks.forEach(el=> {
 				touchpadSupport: true
 			})
 		}
-
-
+    
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
